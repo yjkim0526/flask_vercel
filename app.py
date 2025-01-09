8b477b834ad79af1e5fd2531737193e5fecc3763
yujin kim <koiforever0526@gmail.com>
2025-01-09 12:41:27 +0900

    save app

--- app.py
+++ app.py
@@ -31,5 +31,6 @@ def event_handler(json):
      
 if __name__ == "__main__":
     # app.run(debug=True, host='0.0.0.0', port="6700")
-    socketio.run(app, host='0.0.0.0', port="5000", debug=True)
+    # socketio.run(app, host='0.0.0.0', port="5000", debug=True)
+    socketio.run(app)
 
